c672a453e9cbb1239b2b10dc718126bcc912488d
brzozka <brzozka108@gmail.com>
2023-12-09 20:39:33 +0100

    Interface Structure update
    As in title.

--- InclusiFit/src/components/Navbar.vue
+++ InclusiFit/src/components/Navbar.vue
@@ -1,13 +1,17 @@
+<script setup>
+
+</script>
 <template>
-  <div class="navbar">
-    <h1>Navbar</h1>
-  </div>
+  <header class="w-full bg-indigo-100">
+  <nav class="mx-auto flex justify-between  p-3">
+    <div><span class="text-4xl font-bold px-6 py-3">InclusiFit</span></div>
+    
+    <div class="text-3xl font-semibold px-7  ">
+      <span class="px-3 hover:text-violet-600">Map</span>
+      <span class="px-6 hover:text-violet-600">Exercises</span>
+    </div>
+  </nav>
+</header>
 </template>
 
-<style scoped>
-.navbar {
-  height: 15%; /* 15% of the viewport height */
-  width: 100%; /* 100% of the viewport width */
-  background-color: #f0f0f0; /* Example background color */
-}
-</style>
+
--- InclusiFit/src/views/HomeView.vue
+++ InclusiFit/src/views/HomeView.vue
@@ -3,7 +3,12 @@ import Navbar from '../components/Navbar.vue'
 </script>
 
 <template>
-    <div><Navbar/>
-    <h1>Home View</h1>
-    </div>
+    <div class="flex h-fit flex-col w-full">
+    <div class="flex h-fit flex-col w-[80vw] font-sans mx-auto  "><Navbar></Navbar></div>
+        <div class="flex  h-[80vh] w-[80vw] mx-auto bg-emerald-200"><p class="font-mono">first</p></div>
+        <div class="flex  h-[15vh] w-full  bg-violet-300">second</div>
+
+        <div class="flex w-[80vw] h-[80vh] mx-auto bg-slate-300">third</div>
+    
+</div>
 </template>
