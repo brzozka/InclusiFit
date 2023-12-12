eda2bfb26fdd89b5a731c5ded2c8bef55b5e4579
brzozka <brzozka108@gmail.com>
2023-12-12 21:58:53 +0100

    Update to the design
    As in title.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
 
 </script>
 <template>
-  <header class="w-full bg-indigo-100">
+  <header class="w-full bg-white">
   <nav class="mx-auto flex justify-between  p-3">
     <div><span class="text-4xl font-bold px-6 py-3">InclusiFit</span></div>
     
