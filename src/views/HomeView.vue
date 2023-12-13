61f2ed04a3afa53b1f9931b153b13ba017697ff9
brzozka <brzozka108@gmail.com>
2023-12-13 01:45:38 +0100

    Update HomeView.vue
    As in title.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,12 +16,34 @@ import Navbar from '../components/Navbar.vue'
             </div>
         </div>
         <div class="flex  h-[15vh] w-full  bg-[#655CFE]">
-            <div class="flex h-fit  w-[70vw] font-sans mx-auto items-center">
-                <h1 class="text-3xl font-bold tracking-tight text-white">Making fitness accessible for everyone's needs.</h1>
+            <div class="flex h-fit  w-[70vw] font-sans mx-auto items-center justify-between py-8 px-4">
+                <h1 class="text-3xl font-bold tracking-tight text-white">Making fitness accessible for <br> everyone's needs.</h1>
+                <p class="text-xl  text-white">Personalized exercises<br> for Individual needs</p>
+                <p class="text-xl   text-white">Accessible Gyms in <br>Your Location</p>
             </div>
         </div>
 
-        <div class="flex w-[70vw] h-[80vh] mx-auto bg-slate-300">third</div>
+        <div class="flex h-[80vh] w-[70vw] mx-auto bg-white items-center justify-center">
+            <div class="w-1/2 h-full flex flex-col justify-center items-center">
+                <h1 class="text-4xl font-bold tracking-tight text-gray-800 sm:text-5xl">Inclusive Fitness for Diverse Abilities</h1>
+                <p class="text-2xl">Unleashing fitness resources for people with diverse needs, offering tailored exercises and mapping accessible gyms.</p>
+                <button class="bg-violet-500 hover:bg-violet-700 text-white font-bold text-xl py-2 px-4 rounded-full mt-4">Get Started</button>
+            </div>
+            <div class="w-1/2 h-full flex flex-col justify-center items-center ">
+                <img src="../assets/exercise.jfif" alt="exercise" class="w-fit h-fit py-2 px-4 rounded-[50px]">
+            </div>
+        </div>
+        <div class="flex h-[80vh] w-[70vw] mx-auto bg-white items-center justify-center">
+            <div class="w-1/2 h-full flex flex-col justify-center items-center ">
+                <img src="../assets/exercise.jfif" alt="exercise" class="w-fit h-fit py-2 px-4 rounded-[50px]">
+            </div>
+            <div class="w-1/2 h-full flex flex-col justify-center items-center">
+                <h1 class="text-4xl font-bold tracking-tight text-gray-800 sm:text-5xl">Inclusive Fitness for Diverse Abilities</h1>
+                <p class="text-2xl">Unleashing fitness resources for people with diverse needs, offering tailored exercises and mapping accessible gyms.</p>
+                <button class="bg-violet-500 hover:bg-violet-700 text-white font-bold text-xl py-2 px-4 rounded-full mt-4">Get Started</button>
+            </div>
+            
+        </div>
     
 </div>
 </template>
